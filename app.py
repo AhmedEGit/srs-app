97afcdea330d1c3f4ddc61aac7232a43a0a1ce43
ahmedloria <echbarbi.1234@gmail.com>
2026-07-06 23:46:44 +0200

    ajout du contenu du fichier

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("Hello world !")
+
+dataf = pd.DataFrame({"a": [1, 2, 3], "b": [9, 8, 7]})
+st.dataframe(dataf)
+
+user_input = st.text_area("Entrez votre requête SQL :")
+
+if user_input.strip():
+    try:
+        user_query = duckdb.query(user_input).df()
+        st.write("Voici le résultat :")
+        st.dataframe(user_query)
+    except Exception as e:
+        st.error(f"Erreur SQL : {e}")
+
+
