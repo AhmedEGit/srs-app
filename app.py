3ab879c28f99183c246586da67e35168faa6cc03
ahmedloria <echbarbi.1234@gmail.com>
2026-08-03 23:41:48 +0200

    changed .gitignore encoding and cleaning CI

--- app.py
+++ app.py
@@ -1,25 +1,70 @@
-import streamlit as st
-import pandas as pd
+
+
+import io
+import string
+
 import duckdb
+import pandas as pd
+import streamlit as st
 
 st.write("""
 # SQL revision
 Spaced repetition system SQL
 """)
-# with st.sidebar:
-#     option = st.selectbox("What would you like to revise ?", ["joins", "group by","window functions"])
-#     st.write("you choose", option)
-dataf = pd.DataFrame({"a": [1, 2, 3], "b": [9, 8, 7]})
-st.dataframe(dataf)
 
-user_input = st.text_area("Entrez votre requête SQL :")
+csv1 = """
+beverage, price
+juice, 2.5
+tea, 2
+coffee, 1.5
+"""
 
-if user_input.strip():
+csv2 = """
+food_item, food_price
+croissant juice, 3.5
+chocolatine, 1.5
+cookie, 1.5
+"""
+
+beverages = pd.read_csv(io.StringIO(csv1))
+food_items = pd.read_csv(io.StringIO(csv2))
+
+
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to revise ?", ["joins", "group by", "window functions"]
+    )
+    st.write("you selected", option)
+
+
+query = st.text_area("Entrez votre requête SQL :", key="user_input")
+
+answer = "SELECT * FROM beverages CROSS JOIN food_items"
+solution = duckdb.query(answer).df()
+
+if query:
+    result = duckdb.query(query).df()
+    st.dataframe(result)
     try:
-        user_query = duckdb.query(user_input).df()
-        st.write("Voici le résultat :")
-        st.dataframe(user_query)
-    except Exception as e:
-        st.error(f"Erreur SQL : {e}")
+        result = result[solution.columns]
+        st.dataframe(result.compare(solution))
+    except KeyError as e:
+        st.write("Some columns are missing")
 
+    n_lines_differences = abs(result.shape[0] - solution.shape[0])
+    if n_lines_differences != 0:
+        st.write(
+            f"Your result has {n_lines_differences} lines differences with the solution"
+        )
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
+with tab2:
+    st.write("Table : beverages")
+    st.dataframe(beverages)
+    st.write("Table: food_items")
+    st.dataframe(food_items)
+    st.write("expected :")
+    st.dataframe(solution)
+
+with tab3:
+    st.write(answer)
